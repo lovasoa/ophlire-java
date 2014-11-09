4a4d10b7f769320e068415d92846a9381e787d98
Ophir LOJKINE <pere.jobs@gmail.com>
2014-11-09 04:37:27 +0100

    Create site

--- src/lecteurString/Consumable.java
+++
@@ -1,8 +0,0 @@
-package lecteurString;
-
-public abstract class Consumable {
-	public abstract int matchedChars(LecteurString lect);
-	public boolean matches(LecteurString l) {
-		return matchedChars(l) >= 0;
-	}
-}
